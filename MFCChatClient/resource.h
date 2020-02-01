aa405e50b695cb9e1962c5952821152c5aaae2a8
大白 <@DESKTOP-49U273M>
2020-02-01 22:53:06 +0800

    机械代码

---
+++ MFCChatClient/resource.h
@@ -0,0 +1,33 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MFCChatClient.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCCHATCLIENT_DIALOG        102
+#define IDR_MAINFRAME                   128
+#define IDC_BREAK_BTN                   1000
+#define IDC_CONNECT_BTN                 1001
+#define IDC_CLS_BTN                     1002
+#define IDC_SENDMSG_BTN                 1003
+#define IDC_EDIT1                       1004
+#define IDC_EDIT2                       1005
+#define IDC_PORT_EDIT                   1006
+#define IDC_LIST5                       1011
+#define IDC_COMBO1                      1012
+#define IDC_IPADDRESS1                  1013
+#define IDC_IPADDRESS                   1013
+#define IDC_RADIO1                      1014
+#define IDC_AUTOSENDMSG_RADIO           1014
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1015
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
